030fdbfd6ef4320592cb1d2598f7636aabe22583
coderroot2 <udayraut128@gmail.com>
2024-05-16 16:43:42 +0530

    add responsive

---
+++ script.js
@@ -0,0 +1,17 @@
+var buttonOn = document.getElementById("resButtonA");
+var buttonOff = document.getElementById("resButtonB");
+var element = document.getElementById("myDiv");
+
+buttonOn.addEventListener("click", () => {
+  element.classList.add("resClass");
+  element.classList.remove("navList");
+  buttonOff.style.opacity = "1";
+});
+
+buttonOff.addEventListener("click", () => {
+  element.classList.add("navList");
+  element.classList.remove("resClass");
+  buttonOff.style.opacity = "0";
+});
+
+buttonOff.style.opacity = "0";
